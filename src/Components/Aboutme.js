470bd9092d6208351ff5ac5b07208fcbf6e57165
Abhishek Pandey <abhishekpandey1480@gmail.com>
2023-06-04 00:20:58 +0530

    todayswork

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -3,6 +3,7 @@
     import Navbar from "./Navbar";
     import {FaCalendarAlt} from "react-icons/fa"
     import {MdDownload}  from "react-icons/md";
+    import{TfiComment} from "react-icons/tfi"
 
 
 
@@ -44,6 +45,7 @@
                                 
                                 <div className="expcntr">
                                     <h2 className="exph">Experience</h2>
+
                                     <div className="gigcntr">
                                         <div>
                                         <div className="dot">
@@ -51,27 +53,185 @@
                                        </div> <div className="expl"></div>
                                         </div>
                                     
-                                       <div className="gigcntr">
+                                       <div className="gigcn">
                                          <div className="gig1">
                                             <div className="expy"><FaCalendarAlt/>
                                              <div className="expyear">01/2023 - 04/2023</div>
                                              
                                          </div>
                                          <div className="cmpnyt">SEO Head - Gigzoe</div>
-                                         <div>Gigzoe is freelancer company, also it gives service for your business.</div>
+                                         <div className="work-discription">Gigzoe is freelancer company, also it gives service for your business.</div>
                                          </div>
                                         
                                       </div>
                                      
+                                    </div>
+                                    <div className="gigcntr">
+                                        <div>
+                                        <div className="dot">
+                                       
+                                       </div> <div className="expl"></div>
+                                        </div>
+                                    
+                                       <div className="gigcn">
+                                         <div className="gig1">
+                                            <div className="expy"><FaCalendarAlt/>
+                                             <div className="expyear">07/2023 - 09/2023</div>
+                                             
+                                         </div>
+                                         <div className="cmpnyt">Digital Marketing - Gobookmart</div>
+                                         <div className="work-discription">
+                                         It is a print media company, they review the best books and conduct interviews with the authors.
 
+                                         </div>
+                                         </div>
+                                        
+                                      </div>
+                                     
+                                    </div>
+                                    <div className="dot"></div>
+                                    <div className="expl"></div>
+
+
+                                </div>
+                                <div className="educntr">
+                                    <h2 className="eduh">Education</h2>
+                                    <div className="gigcntr">
+                                        <div>
+                                        <div className="dot">
+                                       
+                                       </div> <div className="edul"></div>
+                                        </div>
+                                    
+                                       <div className="gigcn">
+                                         <div className="gig1">
+                                            <div className="expy"><FaCalendarAlt/>
+                                             <div className="expyear">07/2019 -  Present</div>
+                                             
+                                         </div>
+                                         <div className="cmpnyt">Mathematics & Computing</div><div>Birla Institute Of Technology</div>
+                                         <div className="work-discription-edu">Courses
+                                         <div>Mathematics & Computing</div>
+                                         </div>
+                                         </div>
+                                        
+                                      </div>
+                                     
+                                    </div>
+                                    <div className="gigcntr">
+                                        <div>
+                                        <div className="dot">
+                                       
+                                       </div> <div className="edul"></div>
+                                        </div>
+                                    
+                                       <div className="gigcn">
+                                         <div className="gig1">
+                                            <div className="expy"><FaCalendarAlt/>
+                                             <div className="expyear">03/2017 -  03/2019</div>
+                                             
+                                         </div>
+                                         <div className="cmpnyt">Higher Secondary (HSC)</div><div>Rajkamal Saraswati Vidhaya Mandir</div>
+                                         <div className="work-discription-edu"> Dhanbad, Jharkhand, Pincode - 826001.
+                                         
+                                         </div>
+                                         </div>
+                                        
+                                      </div>
+                                     
+                                    </div>
+                                    <div className="gigcntr">
+                                        <div>
+                                        <div className="dot">
+                                       
+                                       </div> <div className="expl"></div>
+                                        </div>
+                                    
+                                       <div className="gigcn">
+                                         <div className="gig1">
+                                            <div className="expy"><FaCalendarAlt/>
+                                             <div className="expyear">03/2009 -  03/2017</div>
+                                             
+                                         </div>
+                                         <div className="cmpnyt">Secondary School (SSC)</div><div>Indian School of Learning</div>
+                                         <div className="work-discription-edu"> Dhanbad, Jharkhand, Pincode - 826001.
+                                         
+                                         </div>
+                                         </div>
+                                        
+                                      </div>
+                                     
+                                    </div>
+
+                                
+                                </div>
+                            </div>
+                            <div className="skillcntr">
+                                <h2>SKILLS</h2>
+                                <div className="skills-meter">
+                                    <div className="skill-html">
+                                        <span className=""><div className="skill-h">HTML</div></span>
+                                        <div className="item-progress"></div>
+                                       
+                                        
+                                           <TfiComment className="percent-icon"/>
+                                            <span className="percentage">80%</span>
+                                       
+                                      
+                                        <div className="item-line"></div>
+                                    </div>
+                                    <div className="skill-html2">
+                                        <span className=""><div className="skill-h">HTML</div></span>
+                                        <div className="item-progress"></div>
+                                       
+                                        
+                                           <TfiComment className="percent-icon"/>
+                                            <span className="percentage">80%</span>
+                                       
+                                      
+                                        <div className="item-line"></div>
+                                    </div>
+                                     <div className="skill-html3">
+                                        <span className=""><div className="skill-h">HTML</div></span>
+                                        <div className="item-progress"></div>
+                                       
+                                        
+                                           <TfiComment className="percent-icon"/>
+                                            <span className="percentage">80%</span>
+                                       
+                                      
+                                        <div className="item-line"></div>
+                                    </div>
+                                    <div className="skill-html4">
+                                        <span className=""><div className="skill-h">HTML</div></span>
+                                        <div className="item-progress"></div>
+                                       
+                                        
+                                           <TfiComment className="percent-icon"/>
+                                            <span className="percentage">80%</span>
+                                       
+                                      
+                                        <div className="item-line"></div>
+                                    </div>
+                                    <div className="skill-html4">
+                                        <span className=""><div className="skill-h">HTML</div></span>
+                                        <div className="item-progress"></div>
+                                       
+                                        
+                                           <TfiComment className="percent-icon"/>
+                                            <span className="percentage">80%</span>
+                                       
+                                      
+                                        <div className="item-line"></div>
                                     </div>
 
 
                                 </div>
-                                <div className="educntr"><h2 className="eduh">Education</h2></div>
                             </div>
                             
+                            
                     </div>
+                   
                 
 
                     

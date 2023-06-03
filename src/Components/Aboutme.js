d6f8b6fc92701337d07640bb335aa1fa15dbbd8c
Abhishek Pandey <abhishekpandey1480@gmail.com>
2023-06-03 21:34:06 +0530

    change

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -1,51 +1,83 @@
-import React from "react";
-import "./Aboutme.css";
-import Navbar from "./Navbar";
-import {MdDownload}  from "react-icons/md";
+    import React from "react";
+    import "./Aboutme.css";
+    import Navbar from "./Navbar";
+    import {FaCalendarAlt} from "react-icons/fa"
+    import {MdDownload}  from "react-icons/md";
 
 
 
-export default function Abhoutme (){
-    return(
-        <>
-            <div className="abtme">
-                <div><Navbar/></div>
-                <div className="hello">
-                  <div className="abt1" >
-               
-                         <h1 className="abth1">ABOUT ME</h1>
-                        <div><div className="hr1"></div> <h3>I Code and Build Beauitful Things, AND I LOVE WHAT I Do </h3>
-                        <div className="hr2"></div></div>
-                   
-                      </div>
-                          <div className="cont1">
-                                <div className="profpic"><div className="inside">HI</div></div>
-                                <div className="abt2">
-                                <div className="p1">
-                                <div className="int2">Name : Abhishek Pandey </div>
-                                <div className="int2">Birthdate : 14-05-2000</div>
-                                <div className="int2">Nationality : Indian</div>
-                                <div className="int2">Address : Dhanbad, Jharkhand, India</div>
+    export default function Abhoutme (){
+        return(
+            <>
+                <div className="abtme">
+                    <div><Navbar/></div>
+                    <div className="hello">
+                    <div className="abt1" >
+                
+                            <h1 className="abth1">ABOUT ME</h1>
+                            <div><div className="hr1"></div> <h3>I Code and Build Beauitful Things, AND I LOVE WHAT I Do </h3>
+                            <div className="hr2"></div></div>
+                    
+                        </div>
+                            <div className="cont1">
+                                    <div className="profpic"><div className="inside">HI</div></div>
+                                    <div className="abt2">
+                                    <div className="p1">
+                                    <div className="int2">Name : Abhishek Pandey </div>
+                                    <div className="int2">Birthdate : 14-05-2000</div>
+                                    <div className="int2">Nationality : Indian</div>
+                                    <div className="int2">Address : Dhanbad, Jharkhand, India</div>
+                                    </div>
+                                    <div className="p1">
+                                    <div className="int2">Language : English , Hindi</div>
+                                    <div className="int2">Phonr number : 6203321160</div>
+                                    <div className="int2">Email : abhishekpandey1480@gmail.com</div>
+                                    <div className="int2">Github : https://github.com/Abhishek2262</div>
+                                    </div>
+                                    <button className="cvbtn"> <MdDownload className="icon"/> DOWNLOAD MY CV</button>
+                            </div>
+                        </div>
+                            
+                            <div className="hr3"></div> 
+                            
+                            <div className="resumecntr">
+                                
+                                <div className="expcntr">
+                                    <h2 className="exph">Experience</h2>
+                                    <div className="gigcntr">
+                                        <div>
+                                        <div className="dot">
+                                       
+                                       </div> <div className="expl"></div>
+                                        </div>
+                                    
+                                       <div className="gigcntr">
+                                         <div className="gig1">
+                                            <div className="expy"><FaCalendarAlt/>
+                                             <div className="expyear">01/2023 - 04/2023</div>
+                                             
+                                         </div>
+                                         <div className="cmpnyt">SEO Head - Gigzoe</div>
+                                         <div>Gigzoe is freelancer company, also it gives service for your business.</div>
+                                         </div>
+                                        
+                                      </div>
+                                     
+
+                                    </div>
+
+
                                 </div>
-                                <div className="p1">
-                                <div className="int2">Language : English , Hindi</div>
-                                <div className="int2">Phonr number : 6203321160</div>
-                                <div className="int2">Email : abhishekpandey1480@gmail.com</div>
-                                <div className="int2">Github : https://github.com/Abhishek2262</div>
-                                </div>
-                                <button className="cvbtn"> <MdDownload className="icon"/> DOWNLOAD MY CV</button>
-                          </div>
-                          </div>
-                          
-                         <div className="hr3"></div> 
-                         <div>HELELO</div>
+                                <div className="educntr"><h2 className="eduh">Education</h2></div>
+                            </div>
+                            
+                    </div>
+                
+
+                    
+                    
+                    
                 </div>
-               
-
-                
-                
-                
-            </div>
-        </>
-    );
-}
+            </>
+        );
+    }
